b617a5e63af1d2a529831329d4c72984e527d701
Pascal Buhl <pa741buh@htwg-konstanz.de>
2023-05-25 12:42:07 +0200

    test

--- assets/vue/address.js
+++ assets/vue/address.js
@@ -1,42 +1,50 @@
 export default {
-    data() {
-      return {
-        name: "",
-        address: ""
-      };
+  data() {
+    return {
+      name: "",
+      address: "",
+      rules: [
+        val => {
+          const specialChars =
+          '[`!@#$%^&*()_+-=[]{};\':"\\|<>/?~]/';
+          if (specialChars
+            .split('')
+            .some((specialChar) => val.includes(specialChar))) {
+              return "Der Name enthält Sonderzeichen! ";
+            } else {
+            return true;
+          }
+        },
+      ],
+    };
+  },
+  methods: {
+    submitAddress() {
+      let url = new URL(origin + "/api/address");
+      let data = new FormData();
+      data.append("name", this.name);
+      data.append("address", this.address);
+      fetch(url, {
+        method: "POST",
+        body: data,
+      }).then((result) => {
+        this.$router.push("/checkout");
+      });
     },
-    methods: {
-      submitAddress() {
-        let url = new URL(origin + "/api/address");
-        let data = new FormData();
-        data.append("name", this.name);
-        data.append("address", this.address);
-        fetch(url, {
-          method: "POST",
-          body: data,
-        }).then((result) => {
-          this.$router.push("/checkout");
-        });
-      }
-    },  
-    template: `
+  },
+  template: `
     <div class="container">
       <h1>Lieferdaten</h1>
-      <form>
-        <div>
-            <div class="form-group">
-                <label class="col-form-label-lg">Name</label>
-                <input type="text" class="form-control" maxlength="80" v-model="name">
-            </div>
-            <div class="form-group">
-                <label class="col-form-label-lg">Adresse</label>
-                <input type="text" class="form-control" maxlength="120" v-model="address">
-            </div>
-        </div>
-      </form>
+      <v-sheet class="mx-auto">
+        <v-form>
+          <v-text-field v-model="name" :rules="rules" label="Name"></v-text-field>
+          <v-text-field v-model="address" :rules="rules" label="Adresse"></v-text-field>
+        </v-form>
+      </v-sheet>
+
       <router-link class="btn btn-secondary mr-5 " to="/">Weiter einkaufen</router-link>
       <span class="btn btn-primary" @click="submitAddress">Bestellen</span>
       </div>
       `,
-  };
+};
   
